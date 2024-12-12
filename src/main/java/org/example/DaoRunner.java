c725a6b738bc3affd44d4c846ea59874c76a1ae6
Ed Gluschenko <edgluschenko3@gmail.com>
2024-12-12 18:00:34 +0200

    new methods

--- src/main/java/org/example/DaoRunner.java
+++ src/main/java/org/example/DaoRunner.java
@@ -7,26 +7,46 @@ import java.math.BigDecimal;
 
 public class DaoRunner {
     public static void main(String[] args) {
+
+            var students = StudentDao.getInstance().findAll();
+        System.out.println(students);
+
+
+        //updateTest();
+
+
+        //saveTest();
+        //deleteTest();
+    }
+
+    private static void deleteTest() {
         var studentDao= StudentDao.getInstance();
-        var maybeStudent=studentDao.findById(10L);
+        var deletedStudent = studentDao.delete(8L);
+        System.out.println(deletedStudent);
+    }
+
+    private static void saveTest() {
+        var studentDao= StudentDao.getInstance();
+        var student = new Student();
+        student.setFirstName("Viktoria");
+        student.setLastName("Romanova");
+        student.setCourse(3);
+        student.setGradeMathemetic(11.0);
+        student.setGradeHistory(10.5);
+        student.setGradePhysics(11.9);
+        student.setGradeChemistry(9.9);
+        student.setSchoolId(1);
+        var savedStudent = studentDao.save(student);
+        System.out.println(savedStudent);
+    }
+
+    private static void updateTest() {
+        var studentDao = StudentDao.getInstance();
+        var maybeStudent = studentDao.findById(10L);
         System.out.println(maybeStudent);
-      // maybeStudent.ifPresent(student -> {student.setGradeMathemetic(Double.valueOf(11.0));
-        //    studentDao.update(student);
-       // });
-
-
-//        var student = new Student();
-//        student.setFirstName("Viktoria");
-//        student.setLastName("Romanova");
-//        student.setCourse(3);
-//        student.setGradeMathemetic(11.0);
-//        student.setGradeHistory(10.5);
-//        student.setGradePhysics(11.9);
-//        student.setGradeChemistry(9.9);
-//        student.setSchoolId(1);
-//       var savedStudent = studentDao.save(student);
-//       System.out.println(savedStudent);
-        //var deletedStudent = studentDao.delete(8L);
-        //System.out.println(deletedStudent);
+        maybeStudent.ifPresent(student -> {
+            student.setGradeMathemetic(Double.valueOf(2.0));
+            studentDao.update(student);
+        });
     }
 }
